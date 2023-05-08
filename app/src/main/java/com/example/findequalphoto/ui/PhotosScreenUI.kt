f2ec113f9243d03f06e05a21b4e2a4811789c2b5
Николай Мокляк <nikoly555mokorl@yandex.ru>
2023-05-08 18:57:11 +0300

    добавил ui для 2 экрана

--- app/src/main/java/com/example/findequalphoto/ui/PhotosScreenUI.kt
+++ app/src/main/java/com/example/findequalphoto/ui/PhotosScreenUI.kt
@@ -1,46 +1,46 @@
 package com.example.findequalphoto.ui
 
-import android.app.Activity
-import android.graphics.BitmapFactory
-import android.graphics.ImageDecoder
-import android.os.Build
-import android.provider.MediaStore
-import androidx.compose.foundation.Image
+
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.input.KeyboardType.Companion.Uri
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
-import coil.compose.rememberAsyncImagePainter
-import coil.compose.rememberImagePainter
 import com.example.findequalphoto.MainActivity
 import com.example.findequalphoto.NavHost
 import com.example.findequalphoto.data.Photo
+import com.example.findequalphoto.ui.component.AppButtonUI
+import com.example.findequalphoto.ui.component.HeaderTextUI
 import com.example.findequalphoto.ui.theme.FindEqualPhotoTheme
 
 @Composable
 fun PhotosScreenUI(
     navController: NavController,
-    vm: MainVM = viewModel(factory = MainVM.getMainVM(
-        (LocalContext.current as MainActivity).repo))
+    vm: MainVM = viewModel(
+        factory = MainVM.getMainVM(
+            (LocalContext.current as MainActivity).repo
+        )
+    )
 ) {
     val stateUI = vm.statePhoto.collectAsState()
 
@@ -58,92 +58,203 @@ fun PhotosScreenUI(
     isDelete: () -> Unit,
     isSelectPhoto: (photo: Photo, indexPhotos: Int) -> Unit
 ) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(horizontal = 19.dp)
+    Surface(
+        color = MaterialTheme.colors.background,
+        contentColor = MaterialTheme.colors.onBackground
     ) {
-        Text("Похожие фотографии")
 
-        Row(modifier = Modifier.fillMaxWidth()) {
-            Text(text = "Будет доступно после удаления выбранных похожих фотографий")
-            Text(text = "X гб")
-        }
-        if (stateUI is StateUI.Loaded) {
-            LazyColumn(
-                verticalArrangement = Arrangement.spacedBy(11.dp), modifier = Modifier.weight(1f)
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 19.dp)
+        ) {
+            HeaderTextUI("Похожие фотографии")
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 13.dp)
             ) {
-                itemsIndexed(
-                    items = stateUI.photos,
-                    key = { _, photo -> photo.first().uri }) { indexPhotos, photos ->
-                    Column {
-                        var rows = photos.size / 3
-                        if (photos.size.mod(3) > 0) {
-                            rows += 1
-                        }
-                        for (rowId in 0 until rows) {
-                            val firstIndex = rowId * 3
-                            Row {
-                                for (columnId in 0 until 3) {
-                                    val index = (firstIndex + columnId)
-                                    Box(
-                                        Modifier
-                                            .weight(1f)
-                                            .aspectRatio(1f)
-                                    ) {
-                                        if (index < photos.size) {
-                                            Card(modifier = Modifier.clickable {
-                                                isSelectPhoto(photos[index], indexPhotos)
-                                            }) {
-                                                AsyncImage(
-                                                    model = photos[index].uri,
-                                                    contentDescription = null,
-                                                    contentScale = ContentScale.Crop
-                                                )
-                                            }
-                                            Checkbox(
-                                                checked = photos[index].isSelect,
-                                                onCheckedChange = {
-                                                    isSelectPhoto(photos[index], indexPhotos)
-                                                },
-                                                modifier = Modifier.align(Alignment.BottomEnd)
-                                            )
-                                        }
+                Text(
+                    text = "Будет доступно после удаления выбранных похожих фотографий",
+                    style = MaterialTheme.typography.body2
+                )
+                Row() {
+                    Text(text = "X", style = MaterialTheme.typography.h3.copy(fontSize = 38.sp))
+                    Text(text = "гб", style = MaterialTheme.typography.body2.copy(fontSize = 12.sp))
+                }
+
+            }
+            if (stateUI is StateUI.Loaded) {
+                LazyColumn(
+                    verticalArrangement = Arrangement.spacedBy(11.dp),
+                    modifier = Modifier.weight(1f)
+                ) {
+                    itemsIndexed(
+                        items = stateUI.photos,
+                        key = { _, photo -> photo.first().uri }) { indexPhotos, photos ->
+                        Column {
+                            var rows = photos.size / 3
+                            if (photos.size.mod(3) > 0) {
+                                rows += 1
+                            }
+                            for (rowId in 0 until rows) {
+                                val firstIndex = rowId * 3
+                                Row(
+                                    modifier = Modifier.padding(
+                                        top =
+                                        if (rowId != 0)
+                                            11.dp else 0.dp
+                                    )
+                                ) {
+                                    for (columnId in 0 until 3) {
+                                        CardPhotoUI(
+                                            firstIndex,
+                                            columnId,
+                                            photos,
+                                            isSelectPhoto,
+                                            indexPhotos,
+                                            Modifier
+                                                .weight(1f)
+                                                .aspectRatio(1f)
+                                        )
                                     }
+
                                 }
                             }
+                            Spacer(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(vertical = 16.dp)
+                                    .height(1.dp)
+                                    .background(Color(0xFF393B4A))
+                            )
                         }
-                        Spacer(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .height(1.dp)
-                                .background(MaterialTheme.colors.onBackground)
-                        )
                     }
                 }
-            }
-            Column(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Button(onClick = isDelete, Modifier.fillMaxWidth()) {
-                    Text(text = "Удалить")
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    AppButtonUI(text = "Удалить", onClick = isDelete, Modifier.fillMaxWidth())
+                    TextButton(onClick = { /*TODO*/ }, Modifier.fillMaxWidth()) {
+                        Text(text = "Пропустить", color = MaterialTheme.colors.onBackground)
+                    }
                 }
-                TextButton(onClick = { /*TODO*/ }, Modifier.fillMaxWidth()) {
-                    Text(text = "Пропустить")
-                }
-            }
 
 
+            }
         }
     }
 }
 
+@Composable
+private fun CardPhotoUI(
+    firstIndex: Int,
+    columnId: Int,
+    photos: List<Photo>,
+    isSelectPhoto: (photo: Photo, indexPhotos: Int) -> Unit,
+    indexPhotos: Int,
+    modifier: Modifier
+) {
+    val index = (firstIndex + columnId)
+    if (columnId == 1) {
+        Spacer(
+            modifier = Modifier
+                .width(11.dp)
+                .background(Color.Transparent)
+        )
+    }
+    Box(
+        modifier
+    ) {
+        if (index < photos.size) {
 
+
+            val modifier = if (photos[index].isSelect) Modifier
+                .border(
+                    width = 1.dp,
+                    color = MaterialTheme.colors.primary,
+                    shape = RoundedCornerShape(10.dp)
+                )
+                .background(Color.Black.copy(alpha = 0.6f)) else Modifier
+
+            Card(shape = RoundedCornerShape(10.dp),
+                modifier = modifier
+                    .padding(2.dp)
+                    .clickable {
+                        isSelectPhoto(
+                            photos[index],
+                            indexPhotos
+                        )
+                    }) {
+                Box() {
+                    AsyncImage(
+                        model = photos[index].uri,
+                        contentDescription = null,
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier.fillMaxSize()
+                    )
+                    if (photos[index].isSelect) {
+                        Spacer(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .background(Color.Black.copy(alpha = 0.2f)),
+                        )
+                    }
+                }
+
+
+            }
+            if (photos[index].isSelect) {
+                Box(
+                    modifier = Modifier
+                        .padding(end = 8.dp, bottom = 8.dp)
+                        .align(Alignment.BottomEnd)
+                        .size(18.dp)
+                        .border(
+                            width = 2.dp,
+                            color = MaterialTheme.colors.primary,
+                            shape = RoundedCornerShape(5.dp)
+                        )
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Check,
+                        contentDescription = null,
+                        tint = MaterialTheme.colors.primary,
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(3.dp)
+                    )
+
+                }
+
+            }
+        }
+    }
+    if (columnId == 1) {
+        Spacer(
+            modifier = Modifier
+                .width(11.dp)
+                .background(Color.Transparent)
+        )
+    }
+}
+
+@Preview(showBackground = true)
 @Composable
 private fun PhotosScreenUIPreview() {
     FindEqualPhotoTheme {
-
+        PhotosScreenUI(StateUI.Loaded(
+            listOf(
+                listOf(
+                    Photo(android.net.Uri.EMPTY, "", 0, true),
+                    Photo(android.net.Uri.EMPTY, "", 0, false),
+                    Photo(android.net.Uri.EMPTY, "", 0, true),
+                    Photo(android.net.Uri.EMPTY, "", 0, false)
+                )
+            )
+        ), {}, { _, _ -> })
 
     }
 }
--- app/src/main/java/com/example/findequalphoto/ui/component/HeaderTextUI.kt
+++ app/src/main/java/com/example/findequalphoto/ui/component/HeaderTextUI.kt
@@ -1,12 +1,15 @@
 package com.example.findequalphoto.ui.component
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
-fun HeaderTextUI(text:String){
-    Text(text = text, style = MaterialTheme.typography.h6.copy(fontSize = 20.sp, lineHeight = (23.44).sp))
+fun HeaderTextUI(text:String, modifier: Modifier=Modifier){
+    Text(text = text, style = MaterialTheme.typography.h6.copy(fontSize = 20.sp, lineHeight = (23.44).sp),modifier= modifier.padding(start = 54.dp))
 
 }
