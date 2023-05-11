c48b169447c955707a84251e7d620f3dca5fd8b6
Николай Мокляк <nikoly555mokorl@yandex.ru>
2023-05-11 17:57:23 +0300

    удалил, файлы log

--- openCV460/build/intermediates/cxx/Debug/23471dc2/logs/arm64-v8a/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Nikoly\\Program\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Nikoly\\MyProgranAndPet\\Android\\workTest\\FindEqualPhoto\\FindEqualPhoto\\openCV460\\.cxx\\Debug\\23471dc2\\arm64-v8a" ^
-  opencv_jni_shared
